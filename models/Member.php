a985b16c7a26b798f266dd1b849ad3a9b6b84fe0
evgenydmorozov <evgenydmorozov@gmail.com>
2023-04-20 15:10:18 +0500

    feat(models): add Member model

---
+++ models/Member.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Yamobile\Staff\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Member extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+    use \October\Rain\Database\Traits\SoftDelete;
+
+    protected $dates = ['deleted_at'];
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'yamobile_staff_members';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+
+    ];
+}
